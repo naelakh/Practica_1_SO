3b434401f0dc312671f5d1adba999557959a8b68
naelakh <nkx1015@alu.ubu.es>
2026-03-07 21:31:09 +0100

    Update plots_mqtt.py

--- plots_mqtt.py
+++ plots_mqtt.py
@@ -1,11 +1,12 @@
 import json
 import os
 import matplotlib.pyplot as plt
-
+#archivo generado por el script Bash 
 log_file = "mqtt_capture.log"
+#Altura de la grafica ASCII
 ALTURA_GRAFICA = 20
 
-
+#lee el archivo de log 
 def leer_log_file():
 
     lineas = []
@@ -20,7 +21,7 @@ def leer_log_file():
 
     return lineas
 
-
+#Extraer todos los valores numericos de los mensajes JSON
 def extraer_values(lineas):
 
     values = []
@@ -32,6 +33,7 @@ def extraer_values(lineas):
             text_json = linea.split("Payload:", 1)[1].strip()
 
             try:
+                #convertir el texto JSON en un diccionario de Python
                 data = json.loads(text_json)
 
                 for value in data.values():
@@ -44,9 +46,9 @@ def extraer_values(lineas):
 
     return values
 
-
+#Generar una grafica PNG utilizando matplotlib
 def crear_PNG(values):
-
+    # crear carpeta plots si no existe 
     os.makedirs("plots", exist_ok=True)
 
     plt.plot(values)
@@ -59,7 +61,7 @@ def crear_PNG(values):
 
     print("PNG grafica guardada")
 
-
+#Genera una representacion grafica SCII en la terminal
 def grafica_ASCII(values):
 
     if len(values) == 0:
@@ -67,6 +69,7 @@ def grafica_ASCII(values):
         return
 
     max_value = int(max(values))
+    #ancho de la grafica
     ancho = len(values)
 
     scaled_values = []
@@ -77,10 +80,11 @@ def grafica_ASCII(values):
         scaled_values.append(level)
 
     print("\n Grafica ASCII \n")
-    
+# se dibuja la grafica de arriba hacia abajo
     for y in range(ALTURA_GRAFICA, -1, -1):
 
         linea = ""
+        #eje y
         if y == ALTURA_GRAFICA:
             linea += f"{max_value:>3} |"
         elif y == 0:
@@ -96,7 +100,7 @@ def grafica_ASCII(values):
         
     print(linea)
     print("  +" + "---" * ancho)
-
+    #eje x 
     linea_form = "    "
     for i in range(ancho):
         linea_form += f"{i:>3}"
@@ -104,7 +108,7 @@ def grafica_ASCII(values):
 
     print("\n ultimos valores:", values)    
 
-
+#funcion principal de la programa
 def main():
 
     lineas = leer_log_file()
